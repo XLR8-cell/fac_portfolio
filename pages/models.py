e946bb559916cd7584e62aab54f89570a0823c30
WegenerSteven <121510376+WegenerSteven@users.noreply.github.com>
2026-08-04 12:59:21 +0300

    creat models Project and Skills

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+""" 
+1. Create a Project model with fields: name, description, tech_stack, github_url, live_url, is_featured 
+2. Create a Skill model with fields: name, category, level (Beginner / Intermediate / Expert) 
+3. Make migrations and migrate the database 
+4. Add 5 sample projects via the Django shell: python manage.py shell 
+5. Update your projects view to query the database and pass the results to the template 
+6. Display the projects in projects.html using a {% for project in projects %} loop"""
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    tech_stack = models.CharField()
+    github_url = models.URLField()
+    live_url = models.URLField()
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.github_url}"
+    
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    level = models.Choices('Beginner', 'Intermediate', 'Expert')
+    
+    
+    def __str__(self):
+        return f"{self.skill_name} - {self.category} - {self.level}"
+    
+    
